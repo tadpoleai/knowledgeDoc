9ef2f7c91551f374feb2680808dd5cca42c458d0
Frye.Zhang <frye.zhang@wayz.ai>
2023-08-14 15:16:36 +0800

    optimize user input option, no need upload sap file by default

--- prototype/streamlit_ui.py
+++ prototype/streamlit_ui.py
@@ -12,7 +12,7 @@ import tempfile
 from dotenv import load_dotenv
 load_dotenv()
 
-chat = ChatOpenAI(model_name='gpt-3.5-turbo-16k')
+chat = ChatOpenAI(model_name='gpt-3.5-turbo-16k', temperature=0)
 chain = load_qa_chain(chat, chain_type="stuff")
 
 async def main():
@@ -30,54 +30,58 @@ async def main():
 
     #Creating the chatbot interface
     st.title("SAP insights")
+    st.write("""
+             本问答系统已初步训练某大型制造企业内部SAP操作手册，可支持您直接问答 \n \n
+             同时，可支持您上传新的文档，训练后可实现更准确的问答""")
+
 
     if 'ready' not in st.session_state:
         st.session_state['ready'] = False
 
-    uploaded_file = st.file_uploader("选择一个SAP操作手册", type="md")
+    # 默认选择“否”
+    upload_option = st.selectbox("您想上传SAP操作手册吗?", ["否", "是"], index=0)
     
-    # hybrid_retriever = HybridSearchByWeaviate()
+    if upload_option == "是":
+        uploaded_file = st.file_uploader("选择一个SAP操作手册", type="md")
+        
+        # hybrid_retriever = HybridSearchByWeaviate()
 
-    if uploaded_file:
+        if uploaded_file:
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.md') as tmp:
-            tmp.write(uploaded_file.read())
-            tmp_path = tmp.name
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.md') as tmp:
+                tmp.write(uploaded_file.read())
+                tmp_path = tmp.name
 
-        with st.spinner("Processing..."):
-        # Add your code here that needs to be executed
-            # uploaded_file.seek(0)
-            # file = uploaded_file.read()
-            # # pdf = PyPDF2.PdfFileReader()
-            # vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
-            # qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=vectors.as_retriever(), return_source_documents=True)
-            
-            from langchain.document_loaders import TextLoader
-            #uploaded_file.seek(0)
-            # file = uploaded_file.read()
-            loader = TextLoader(tmp_path)
-            markdown_document = loader.load()
+            with st.spinner("Processing..."):
+                from langchain.document_loaders import TextLoader
+                #uploaded_file.seek(0)
+                # file = uploaded_file.read()
+                loader = TextLoader(tmp_path)
+                markdown_document = loader.load()
 
-            headers_to_split_on = [
-                ("#", "Header_1"),
-                # ("##", "Header_2"),
-                # ("###", "Header_3"),
-            ]
+                headers_to_split_on = [
+                    ("#", "Header_1"),
+                    # ("##", "Header_2"),
+                    # ("###", "Header_3"),
+                ]
 
-            # MD splits
-            from langchain.text_splitter import MarkdownHeaderTextSplitter
-            markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-            print(f'markdown_document: {markdown_document[0]}')
-            
-            #TODO Only support the first document 
-            md_header_splits = markdown_splitter.split_text(markdown_document[0].page_content)
-            
-            hybrid_retriever = HybridSearchByWeaviate()
-            hybrid_retriever.ingest_docments(md_header_splits)
-            
-            #qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=hybrid_retriever.as_retriever(), return_source_documents=True)
+                # MD splits
+                from langchain.text_splitter import MarkdownHeaderTextSplitter
+                markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+                print(f'markdown_document: {markdown_document[0]}')
+                
+                #TODO Only support the first document 
+                md_header_splits = markdown_splitter.split_text(markdown_document[0].page_content)
+                
+                hybrid_retriever = HybridSearchByWeaviate()
+                hybrid_retriever.ingest_docments(md_header_splits)
+                
+                #qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=hybrid_retriever.as_retriever(), return_source_documents=True)
 
 
+            st.session_state['ready'] = True
+    elif upload_option == "否":
+        hybrid_retriever = HybridSearchByWeaviate()
         st.session_state['ready'] = True
 
     st.divider()
@@ -85,38 +89,48 @@ async def main():
     if 'ready' in st.session_state and st.session_state['ready']:
 
         if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["准备就绪! 让AI来帮助您更好的使用SAP系统" + uploaded_file.name]
+            st.session_state['generated'] = ["准备就绪! 让AI来帮助您更好的使用SAP系统"]
 
         if 'past' not in st.session_state:
             st.session_state['past'] = ["你好!"]
 
-        # container for chat history
-        response_container = st.container()
+        if st.session_state['generated']:
+            # container for chat history
+            response_container = st.container()
 
-        # container for text box
-        container = st.container()
+            # container for text box
+            container = st.container()
 
         with container:
             with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="财务预支应该如何处理", key='input')
+                default_question = "怎么判断生产成本月结完成无误"
+                user_input = st.text_input("Query:", placeholder=default_question, key='input')
                 submit_button = st.form_submit_button(label='发送')
+                if not user_input:
+                    user_input = default_question
 
             if submit_button and user_input:
                 #output = await conversational_chat(user_input)
                 # chain = load_qa_chain(chat, chain_type="stuff")
                 # answer = chain.run(input_documents=search_result[0:2], question=question)    
-                
-                print(f'用户的问题是：{user_input}')
-                search_result = hybrid_retriever.search(user_input)
-                if len(search_result) == 0:
-                    output = "很抱歉，没有发现与问题相关的原文内容"
-                else:
-                    print(search_result)
-                    output = chain.run(input_documents=search_result[0:2], question=user_input)            
-                        
-                
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
+                print(f'locals:{locals()}')
+                if 'hybrid_retriever' in locals():
+                    print(f'用户的问题是：{user_input}')
+                    search_result = hybrid_retriever.search(user_input)
+                    if len(search_result) == 0:
+                        output = "很抱歉，没有发现与问题相关的原文内容"
+                    else:
+                        print(search_result)
+                        output = chain.run(input_documents=search_result[0:2], question=user_input)            
+                            
+                    
+                    st.session_state['past'].append(user_input)
+                    st.session_state['generated'].append(output)
+                # else:
+                #     # 直接使用GPT-3.5模型处理用户输入
+                #     output = await conversational_chat(user_input)
+                #     st.session_state['past'].append(user_input)
+                #     st.session_state['generated'].append(output)
 
         if st.session_state['generated']:
             with response_container:
